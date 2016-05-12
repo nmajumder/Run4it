f4fa7a958aa1bca554598fb663a937171af93647
nmajumder <nathan.majumder@tufts.edu>
2016-05-12 13:02:43 +0200

    adding navigation drawer layout and options

--- app/src/main/java/hu/ait/android/run4it/ProfileActivity.java
+++ app/src/main/java/hu/ait/android/run4it/ProfileActivity.java
@@ -79,21 +79,23 @@ public class ProfileActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
+/*
+        if (id == R.id.nav_profile) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_stats) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_goals) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_competitions) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_objectives) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
+        } else if (id == R.id.nav_numPoints) {
+            // do nothing (unclickable)
         }
-
+*/
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
